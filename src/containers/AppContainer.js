8ae7334693f00087663112ed6e8b0ff0ba40e942
Dessie Mompati <dessie.mompati@lotterywest.wa.gov.au>
2019-12-09 17:13:12 +0800

    added fetch

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -6,10 +6,31 @@ import Register from '../pages/Register';
 import Pets from '../pages/Pets';
 // import AboutUs from '../pages/AboutUs';
 
+const API_URL = 'https://api.openweathermap.org/data/2.5';
 
 class AppContainer extends Component {
     constructor(props) {
       super(props);}
+
+      fetchApiData() {
+        fetch(
+          `${API_URL}/weather?q=${this.state.city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        ) // Call the fetch function passing the url of the API as a parameter
+          .then(response => {
+            console.log(response);
+            return response.json();
+            // Your code for handling the data you get from the API
+          })
+          .then(data => {
+            console.log(data);
+            this.setState({ temperature: data.main.temp });
+          })
+          .catch(function(err) {
+            console.error(err);
+            // This is where you run code if the server returns any errors
+          });
+      }
+    
       render(){
           return(
               <>
